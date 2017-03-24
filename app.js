c83728e473554f758822dc66e4b00ed4ba79dd6a
chulbuji-kim <chulbuji.kim@gmail.com>
2017-03-24 18:48:55 +0900

    update console log

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var express = require('express');
 var app = express();
 
 app.listen(3000, function () {
-	console.log('__test server is listenning on port 3000!');
+	console.log('___test server is listenning on port 3000!');
 });
